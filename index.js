bfecf47aa3da5b50595e9a047afb201d4308c681
david.amaya <2014AR603@catolica.edu.sv>
2020-10-08 11:30:27 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import 'react-native-gesture-handler';
-
->>>>>>> 423e92746de6dd702de111b871726560243b6176
 /**
  * @format
  */
@@ -10,27 +5,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-<<<<<<< HEAD
-=======
-import messaging from '@react-native-firebase/messaging';
-
-// Register background handler
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background on index.js!', remoteMessage);
-    
-  });
-
-  function HeadlessCheck({ isHeadless }) {
-    if (isHeadless) {
-      // App has been launched in the background by iOS, ignore
-      return null;
-    }
-  
-    return <App />;
-  }
-  
-  
-  
->>>>>>> 423e92746de6dd702de111b871726560243b6176
 
 AppRegistry.registerComponent(appName, () => App);
